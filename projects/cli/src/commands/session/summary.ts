fea4e9721d3d07957dd0b1d5a1b1bb9b7e8610b5
Van Staub <52075243+van-fs@users.noreply.github.com>
2026-03-25 06:10:49 -0400

    add summary overrides

--- projects/cli/src/commands/session/summary.ts
+++ projects/cli/src/commands/session/summary.ts
@@ -1,5 +1,6 @@
-import { Profile } from '@fullstory/activation-sdk/index.js';
+import { Profile, ProfileConfiguration, Summary } from '@fullstory/activation-sdk/index.js';
 import { Args, Flags } from '@oclif/core';
+import { readFileSync } from 'fs-extra';
 
 import { Command, Fmt, Prompt } from '../../core/index.js';
 
@@ -10,7 +11,14 @@ export default class SessionSummaryCommand extends Command {
   };
 
   static description = `Generate an AI summary of a session using a summarization profile.
-The profile specifies prompting instructions and session context configuration.
+
+A summary profile contains prompting instructions and session context configuration. Profiles can be provided by:
+- Interatively choosing a previously created profile.
+- Providing the previously created profile ID.
+- Providing a local file containing a profile's JSON.
+- Providing both the profile ID and a local file.
+
+When both a profile ID and a local file are used, the local file's profile properties will override the same properties in the profile referenced by the ID.
 
 For more information, see https://developer.fullstory.com/server/sessions/summarize/.`;
 
@@ -18,8 +26,10 @@ For more information, see https://developer.fullstory.com/server/sessions/summar
 
   static examples = [
     { command: '<%= config.bin %> session:summary 1841382665432129521:4929353557192241189', description: 'Interactively select a profile and summarize the session.' },
-    { command: '<%= config.bin %> session:summary 1841382665432129521:4929353557192241189 1c07280f-df08-494f-873e-6214cb6c46b', description: 'Summarize the session using a specific summary profile.' },
+    { command: '<%= config.bin %> session:summary 1841382665432129521:4929353557192241189 1c07280f-df08-494f-873e-6214cb6c46b', description: 'Summarize the session using a saved profile.' },
     { command: '<%= config.bin %> session:summary 1841382665432129521:4929353557192241189 1c07280f-df08-494f-873e-6214cb6c46b --endTimestamp 2024-08-01T13:00:00Z', description: 'Summarize from session start time until the end timestamp' },
+    { command: '<%= config.bin %> session:summary 1841382665432129521:4929353557192241189 --file profile.json', description: 'Summarize the session using a local profile.' },
+    { command: '<%= config.bin %> session:summary 1841382665432129521:4929353557192241189 1c07280f-df08-494f-873e-6214cb6c46b --file profile.json', description: 'Summarize the session using a saved profile with local overrides.' },
   ];
 
   static flags = {
@@ -41,31 +51,86 @@ For more information, see https://developer.fullstory.com/server/sessions/summar
     return selectedProfile;
   }
 
+  /**
+   * Prints the summary response. If a response schema is present, the raw JSON response is printed. Otherwise, the summary text is printed as plaintext.
+   * @param summary The summary object containing the response and summary text.
+   * @param json Whether to print the summary as JSON.
+   * @returns The JSON itself or void if the summary text is printed.
+   */
+  async printSummary(summary: Summary, json = false) {
+    if (summary.response_schema) {
+      return this.logJson(summary.response);
+    }
+
+    // print the raw JSON or the summary text itself as plaintext
+    return json ? summary : this.log(summary.summary);
+  }
+
+
   async run() {
-    const { args: { sessionId, profileId }, flags: { endTimestamp, json } } = await this.parse(SessionSummaryCommand);
+    const { args: { sessionId, profileId }, flags: { endTimestamp, json, file } } = await this.parse(SessionSummaryCommand);
 
     const { SummaryProfile, Session } = this.Fullstory;
 
-    let profile: Profile;
+    let localProfile: ProfileConfiguration | undefined;
 
-    if (profileId) {
-      profile = await SummaryProfile.get(profileId);
-    } else {
-      const profiles = await SummaryProfile.list();
+    if (file) {
+      localProfile = JSON.parse(readFileSync(file, 'utf8'));
 
-      if (profiles.length === 0) {
-        this.error(`No summary profiles found. Create one with ${Fmt.cmd(this.config.bin, 'profile:create')}.`);
+      if (!localProfile) {
+        this.error('Failed to load local profile.');
       }
 
-      profile = await this.chooseProfile(profiles);
+      // below there's some intelligent parsing and reformatting based on file structure
+
+      // the local profile has an id because it's likely from a list profiles response
+      if ((localProfile as Profile).id) {
+        localProfile = {
+          ...(localProfile as Profile).configuration,
+          llm: localProfile.llm,
+        }
+      }
+
+      // the local profile has a name - often because this is used with create profile
+      if ((localProfile as { name?: string }).name) {
+        delete (localProfile as { name?: string }).name;
+      }
     }
 
-    const summary = await Session.summary(sessionId, profile.id, endTimestamp);
+    // use only the local profile and summarize
+    if (localProfile && !profileId) {
+      if (endTimestamp) {
+        localProfile.slice = { ...localProfile.slice, 'end_timestamp': endTimestamp };
+      }
 
-    if (profile.llm.response_schema) {
-      this.logJson(summary.response);
-    } else {
-      return json ? summary : this.log(summary.summary);
+      const summary = await Session.summaryWithOverrides(sessionId, localProfile, profileId);
+      return this.printSummary(summary, json);
     }
+
+    // override a specific profile with the local profile and summarize
+    if (localProfile && profileId) {
+      // NOTE that the profile ID can be supplied directly; there is no need to retrieve the profile
+      const summary = await Session.summaryWithOverrides(sessionId, localProfile, profileId);
+      return this.printSummary(summary, json);
+    }
+
+    // no local profile, use a remote profile and summarize
+    if (profileId) {
+      const summary = await Session.summary(sessionId, profileId, endTimestamp);
+      return this.printSummary(summary, json);
+    }
+
+    // interactively choose a profile and summarize
+    const profiles = await SummaryProfile.list();
+
+    if (profiles.length === 0) {
+      this.error(`No summary profiles found. Create one with ${Fmt.cmd(this.config.bin, 'profile:create')}.`);
+    }
+
+    // ask the user to select one
+    const remoteProfile = await this.chooseProfile(profiles);
+
+    const summary = await Session.summary(sessionId, remoteProfile.id, endTimestamp);
+    return this.printSummary(summary, json);
   }
 }
